bec052721562cda3b899a78a7a36f65e0669b452
Reinaldo Guedes <rguedes.dev@gmail.com>
2025-08-20 18:54:04 -0300

    Mais um ajuste no popunder.

--- frontend/src/app/(pages)/chapter/[id]/page.tsx
+++ frontend/src/app/(pages)/chapter/[id]/page.tsx
@@ -18,6 +18,8 @@ import { AdBlockDetector } from "@/components/AdBlockDetector";
 
 // Components ExoCLick
 import { ExoClickBannerTop } from "@/components/ExoClickBannerTop";
+import { ExoClickPopunder } from "@/components/ExoClickPopunder";
+
 // Icons
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
@@ -30,18 +32,6 @@ function Chapter() {
   const [hentais, setHentais] = useState([]);
   const [user, setUser] = useState({});
   const [subscriptionActive, setSubscriptionActive] = useState(false);
-
-  useEffect(() => {
-    function handleClick() {
-      if (window.popMagic && typeof window.popMagic.preparePop === "function") {
-        window.popMagic.preparePop();
-      }
-    }
-
-    document.addEventListener("click", handleClick);
-
-    return () => document.removeEventListener("click", handleClick);
-  }, []);
 
   // Desabilitar o menu de contexto do botão direito do mouse
   useEffect(() => {
@@ -156,35 +146,7 @@ function Chapter() {
             {subscriptionActive !== "active" && (
               <div className="flex flex-row justify-center items-center">
                 <div className="bg-violet-900 py-8 w-[1200px] text-center font-semibold text-2xl rounded-md">
-                  <Script
-                    id="popunder-exoclick"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                      __html: `
-            (function() {
-              var adConfig = {
-                "ads_host": "a.pemsrv.com",
-                "syndication_host": "s.pemsrv.com",
-                "idzone": 5706214,
-                "popup_fallback": false,
-                "popup_force": false,
-                "chrome_enabled": true,
-                "new_tab": false,
-                "frequency_period": 720,
-                "frequency_count": 1,
-                "trigger_method": 3, 
-                "trigger_class": "",
-                "trigger_delay": 0,
-                "capping_enabled": false,
-                "tcf_enabled": true,
-                "only_inline": false
-              };
-              window.popMagic && window.popMagic.init && window.popMagic.init(adConfig);
-            })();
-          `,
-                    }}
-                  />
-
+                  <ExoClickPopunder />
                   <ExoClickBannerTop
                     zoneId="5706196"
                     className="eas6a97888e2"
--- frontend/src/components/ExoClickPopunder.tsx
+++ frontend/src/components/ExoClickPopunder.tsx
@@ -1,52 +1,38 @@
 "use client";
 
 import { useEffect } from "react";
-import Script from "next/script";
 
 function ExoClickPopunder() {
   useEffect(() => {
-    function handleClick() {
-      if (window.popMagic && typeof window.popMagic.preparePop === "function") {
-        window.popMagic.preparePop();
-      }
-    }
+    const script = document.createElement("script");
+    script.type = "application/javascript";
+    script.async = true;
+    script.innerHTML = `
+      (function() {
+        var adConfig = {
+          "ads_host": "a.pemsrv.com",
+          "syndication_host": "s.pemsrv.com",
+          "idzone": 5706214,
+          "popup_fallback": false,
+          "popup_force": false,
+          "chrome_enabled": true,
+          "new_tab": false,
+          "frequency_period": 720,
+          "frequency_count": 1,
+          "trigger_method": 3,
+          "capping_enabled": false
+        };
+        window.popMagic && window.popMagic.init && window.popMagic.init(adConfig);
+      })();
+    `;
+    document.body.appendChild(script);
 
-    document.addEventListener("click", handleClick);
-
-    return () => document.removeEventListener("click", handleClick);
+    return () => {
+      document.body.removeChild(script);
+    };
   }, []);
 
-  return (
-    <>
-      {/* Script do Popunder */}
-      <Script
-        id="popunder-exoclick"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function() {
-            var adConfig = {
-              "ads_host": "a.pemsrv.com",
-              "syndication_host": "s.pemsrv.com",
-              "idzone": 5706214,
-              "popup_fallback": false,
-              "popup_force": false,
-              "chrome_enabled": true,
-              "new_tab": false,
-              "frequency_period": 720,
-              "frequency_count": 1,
-              "trigger_method": 3, 
-              "trigger_class": "",
-              "trigger_delay": 0,
-              "capping_enabled": false,
-              "tcf_enabled": true,
-              "only_inline": false
-            };
-            window.popMagic && window.popMagic.init && window.popMagic.init(adConfig);
-          })();`,
-        }}
-      />
-    </>
-  );
+  return null; // não precisa renderizar nada
 }
 
 export { ExoClickPopunder };
